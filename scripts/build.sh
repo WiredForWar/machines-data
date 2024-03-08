39f12a5908d33712cd366009491875d479a107c0
Alexander Akulich <akulichalexander@gmail.com>
2024-03-09 01:47:22 +0300

    Add red machines and constructions icons (grayscale them at build time)

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,7 @@ SCRIPT=$(readlink -f $0)
 SCRIPTS_PATH=$(dirname $SCRIPT)
 
 ROOT_DIR="$(dirname $SCRIPTS_PATH)"
+DATA_DIR="$ROOT_DIR/data"
 SOURCE_DIR="$ROOT_DIR/datasrc"
 if [ x"$BUILD_DIR" = "x" ]; then
   BUILD_DIR="$ROOT_DIR/build"
@@ -34,6 +35,14 @@ if [ x"$CONVERTER_COMMAND" = x ]; then
   exit 1
 fi
 
+if [ -x "$(command -v gimp)" ]; then
+  GIMP_COMMAND="gimp"
+  echo "-- Found GIMP$(gimp --version|cut -d n -f 4-)"
+else
+  echo "Please install GIMP (needed e.g. to generate grayscale icons)"
+  exit 1
+fi
+
 convert_svg2png()
 {
   INPUT=$1
@@ -48,10 +57,57 @@ GUI_DIRS=$(find gui -mindepth 1 -type d)
 SVG_FILES=$(find -type f -name '*.svg')
 PLANET_TEXTURES=$(find models -type f -name 'for-planets.txt')
 
+cd "$ROOT_DIR"
+cd data/gui/navigate
+
+cd construc/red
+NAVIGATE_CONSTRUCTION_ICONS=$(find . -type f -name '*.png'| sed 's/^..// ; s/....$//')
+cd ../..
+
+cd machines/red
+NAVIGATE_MACHINES_ICONS=$(find . -type f -name '*.png'| sed 's/^..// ; s/....$//')
+cd ../..
+
 # Do the job
 mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
 
+mkdir -p gui/navigate
+cd gui/navigate/
+
+mkdir -p construc/grey
+mkdir -p machines/grey
+cp -rf $DATA_DIR/gui/navigate/construc/red/*.png construc/grey/
+cp -rf $DATA_DIR/gui/navigate/machines/red/*.png machines/grey/
+
+grayscale_icons()
+{
+  echo "Creating grayscale icons from colorful... (this will take a while)"
+  MACHINES_ORIGIN_BRIGHTNESS=0.155
+  CONSTRUCTIONS_ORIGIN_BRIGHTNESS=0.275
+
+  # Use the average brightness value for both Machines and Constructions
+  BRIGHTNESS=0.215
+  CONTRAST=0.0
+
+  GIMP_COMMAND_PART=""
+  for ICON in $NAVIGATE_CONSTRUCTION_ICONS
+  do
+    GIMP_COMMAND_PART="$GIMP_COMMAND_PART (do-grayscale-icon \"construc/grey/$ICON.png\" \"construc/grey/$ICON.png\" )"
+  done
+
+  for ICON in $NAVIGATE_MACHINES_ICONS
+  do
+    GIMP_COMMAND_PART="$GIMP_COMMAND_PART (do-grayscale-icon \"machines/grey/$ICON.png\" \"machines/grey/$ICON.png\" )"
+  done
+
+  gimp -idf -b "(define (do-grayscale-icon input output) (let* ((image (car (gimp-file-load RUN-NONINTERACTIVE input input) ) ) (drawable (car (gimp-image-get-active-layer image)))) (gimp-image-convert-grayscale image) (gimp-drawable-brightness-contrast drawable $BRIGHTNESS $CONTRAST) (gimp-file-save RUN-NONINTERACTIVE image drawable output output) (gimp-image-delete image)) ) $GIMP_COMMAND_PART" -b '(gimp-quit 0)' 2> /dev/null
+  # Grayscale icons are done
+}
+
+grayscale_icons
+
+cd "$BUILD_DIR"
 for GUI_DIR in $GUI_DIRS
 do
   mkdir -p "$GUI_DIR"
